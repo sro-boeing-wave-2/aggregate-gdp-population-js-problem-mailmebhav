b90f6e3376613896f7cbdd6b92ac921d3cd8c3bc
Bhavin Patel <mailmebhav@gmail.com>
2018-07-16 12:45:31 +0530

    async functions fixed

--- aggregate.js
+++ aggregate.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const continent = require('./continent');
 // This function converts csv to json array of objects.
-function csvtojson(data) {
+async function csvtojson(data) {
   const dataarr = data.replace('"', '').split('\n');
   const result = [];
   const headers = dataarr[0].split(',');
@@ -29,7 +29,7 @@ const final = {
   Africa: { GDP_2012: 0, POPULATION_2012: 0 },
 };
 
-function readFileAsync(filePath) {
+async function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, datavar) => {
       if (err) {
@@ -41,7 +41,7 @@ function readFileAsync(filePath) {
   });
 }
 
-function writeFileAsync(writepath, stringwrite) {
+async function writeFileAsync(writepath, stringwrite) {
   return new Promise((resolve, reject) => {
     fs.writeFile(writepath, stringwrite, 'utf8', (err, datavar) => {
       if (err) {
