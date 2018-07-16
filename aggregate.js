680f0e4ea6a26bac9c7f12f40dbad9c83aa819e0
Bhavin Patel <mailmebhav@gmail.com>
2018-07-16 10:12:39 +0530

    added async to read and write funnctions

--- aggregate.js
+++ aggregate.js
@@ -32,7 +32,7 @@ const final = {
   Africa: { GDP_2012: 0, POPULATION_2012: 0 },
 };
 
-function readFileAsync(filePath) {
+async function readFileAsync(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, datavar) => {
       if (err) {
@@ -44,7 +44,7 @@ function readFileAsync(filePath) {
   });
 }
 
-function writeFileAsync(writepath, stringwrite) {
+async function writeFileAsync(writepath, stringwrite) {
   return new Promise((resolve, reject) => {
     fs.writeFile(writepath, stringwrite, 'utf8', (err, datavar) => {
       if (err) {
